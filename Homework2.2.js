cc13f595a88b1cc565466248aabdce0aafacaae7
Constantin Rusenko <krusenko@gmail.com>
2018-11-02 22:57:20 +0200

    Homework 2.2

--- Homework2.2.js
+++ Homework2.2.js
@@ -1,34 +1,72 @@
-//  Created by Rusenko Constantin on 01.11.2018.
+var array = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
 
-let arr;
-arr = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]; // 1
-let len = arr.length;
 
-function waterAmountLevel(arr) {
-    let waterAmount = 0;
-
-    for (let i = 0; i < len; i++) {
-
-        let leftMax = 0;
-        for(let leftIndex = i-1; leftIndex >= 0; leftIndex-- ) {
-            leftMax = Math.max(leftMax, arr[leftIndex]);
-        }
-
-        let rightMax = 0;
-        for (let rightIndex = i+1; rightIndex < len; rightIndex++ ) {
-            rightMax = Math.max(rightMax,arr[rightIndex]);
-        }
-
-        let minimalLevel = Math.min(leftMax,rightMax);
-
-        if(minimalLevel > arr[i]) {
-            waterAmount += Math.min(leftMax,rightMax) - arr[i];
-        }
-    }
-    console.log(arr);
-    console.log('solution: ',waterAmount);
-    return	waterAmount;
-
+function maxValue(arr) {
+    var max = 0;
+    for (i = 0; i < arr.length; i++)
+        if (max < arr[i])
+            max = arr[i];
+    console.log("Max value: ",max);
 }
-waterAmountLevel(arr);
 
+maxValue(array);
+
+function minValue(arr) {
+    var min = 0;
+    for (i = 0; i < arr.length; i++)
+        if (min > arr[i])
+            min = arr[i];
+    console.log("Max value: ",min);
+}
+
+
+minValue(array);
+
+function sumValue(arr) {
+    var sum = 0;
+    for (i = 0; i < arr.length; i++)
+        sum += arr[i];
+    console.log("Sum value: ",sum);
+}
+
+sumValue(array);
+
+function Replace(arr) {
+    var obj = {};
+    var min = 0;
+    var max = 0;
+
+    var sum = 0;
+    for (i = 0; i < arr.length; i++)
+        sum += arr[i];
+    arr.forEach((elem, i) => {
+        if (min > arr[i]) {
+        min = arr[i];
+    } else if (max < arr[i]) {
+        max = arr[i];
+    }
+});
+    for (var i = 0; i < arr.length; ++i)
+        obj[i] = arr[i];
+    console.log("Min&&Max&&Sum", {min, max, sum}, obj);
+}
+
+Replace(array);
+
+function arrayInObject(arr) {
+    var obj = {};
+    for (var i = 0; i < arr.length; ++i)
+        obj[i] = arr[i];
+    console.log("Array in obj",obj);
+}
+
+arrayInObject(array);
+
+function callbackTest(number, callback) {
+    console.log(number*number);
+    callback();
+};
+
+callbackTest(5, function () {
+    console.log("Hello");
+})
